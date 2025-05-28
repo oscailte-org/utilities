12436178afe44092612787bb243f635baffd03ca
oscailte-org <135216351+oscailte-org@users.noreply.github.com>
2025-05-28 14:39:35 +0100

    Create go_xm.sh

---
+++ crypto/scripts/go_xm.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./xmrig --algo=rx/0 --proxy=127.0.0.1:9050 --url=xmr-eu1.nanopool.org:10300 --user=46ZLUxX822wESdjw1tXbthJUD3mC81g5yNtybh5n8DmPcCn64viUk4E9LnfBrHogNHVMz2H9yrz9QWieZAKuHMRAPsd56Td.workerd02 --pass=x
